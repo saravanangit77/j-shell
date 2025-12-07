48e15ff8773053b08c817044e11f94751364317e
karupal <karuppiah_al@trimble.com>
2025-12-07 12:56:23 +0530

    initial mvn setup

---
+++ src/test/java/com/yourname/shell/AppTest.java
@@ -0,0 +1,19 @@
+package com.yourname.shell;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
